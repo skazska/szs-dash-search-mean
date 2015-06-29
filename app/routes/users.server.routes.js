0b56031108006809acfba619c025d3e66e5a44cb
skazska <skazska@gmail.com>
2015-06-29 17:57:22 +0500

    changed users.server.routes to use Router mounted on '/' and '/admin' to support admin fe auth

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -4,54 +4,58 @@
  * Module dependencies.
  */
 var passport = require('passport');
+var router = require("express").Router();
 
 module.exports = function(app) {
-	// User Routes
-	var users = require('../../app/controllers/users.server.controller');
+  // User Routes
+  var users = require('../../app/controllers/users.server.controller');
 
-	// Setting up the users profile api
-	app.route('/users/me').get(users.me);
-	app.route('/users').put(users.update);
-	app.route('/users/accounts').delete(users.removeOAuthProvider);
+  app.use('/admin', router);
+  app.use(router);
 
-	// Setting up the users password api
-	app.route('/users/password').post(users.changePassword);
-	app.route('/auth/forgot').post(users.forgot);
-	app.route('/auth/reset/:token').get(users.validateResetToken);
-	app.route('/auth/reset/:token').post(users.reset);
+  // Setting up the users profile api
+  router.route('/users/me').get(users.me);
+  router.route('/users').put(users.update);
+  router.route('/users/accounts').delete(users.removeOAuthProvider);
 
-	// Setting up the users authentication api
-	app.route('/auth/signup').post(users.signup);
-	app.route('/auth/signin').post(users.signin);
-	app.route('/auth/signout').get(users.signout);
+  // Setting up the users password api
+  router.route('/users/password').post(users.changePassword);
+  router.route('/auth/forgot').post(users.forgot);
+  router.route('/auth/reset/:token').get(users.validateResetToken);
+  router.route('/auth/reset/:token').post(users.reset);
 
-	// Setting the facebook oauth routes
-	app.route('/auth/facebook').get(passport.authenticate('facebook', {
-		scope: ['email']
-	}));
-	app.route('/auth/facebook/callback').get(users.oauthCallback('facebook'));
+  // Setting up the users authentication api
+  router.route('/auth/signup').post(users.signup);
+  router.route('/auth/signin').post(users.signin);
+  router.route('/auth/signout').get(users.signout);
 
-	// Setting the twitter oauth routes
-	app.route('/auth/twitter').get(passport.authenticate('twitter'));
-	app.route('/auth/twitter/callback').get(users.oauthCallback('twitter'));
+  // Setting the facebook oauth routes
+  router.route('/auth/facebook').get(passport.authenticate('facebook', {
+    scope: ['email']
+  }));
+  router.route('/auth/facebook/callback').get(users.oauthCallback('facebook'));
 
-	// Setting the google oauth routes
-	app.route('/auth/google').get(passport.authenticate('google', {
-		scope: [
-			'https://www.googleapis.com/auth/userinfo.profile',
-			'https://www.googleapis.com/auth/userinfo.email'
-		]
-	}));
-	app.route('/auth/google/callback').get(users.oauthCallback('google'));
+  // Setting the twitter oauth routes
+  router.route('/auth/twitter').get(passport.authenticate('twitter'));
+  router.route('/auth/twitter/callback').get(users.oauthCallback('twitter'));
 
-	// Setting the linkedin oauth routes
-	app.route('/auth/linkedin').get(passport.authenticate('linkedin'));
-	app.route('/auth/linkedin/callback').get(users.oauthCallback('linkedin'));
+  // Setting the google oauth routes
+  router.route('/auth/google').get(passport.authenticate('google', {
+    scope: [
+      'https://www.googleapis.com/auth/userinfo.profile',
+      'https://www.googleapis.com/auth/userinfo.email'
+    ]
+  }));
+  router.route('/auth/google/callback').get(users.oauthCallback('google'));
 
-	// Setting the github oauth routes
-	app.route('/auth/github').get(passport.authenticate('github'));
-	app.route('/auth/github/callback').get(users.oauthCallback('github'));
+  // Setting the linkedin oauth routes
+  router.route('/auth/linkedin').get(passport.authenticate('linkedin'));
+  router.route('/auth/linkedin/callback').get(users.oauthCallback('linkedin'));
 
-	// Finish by binding the user middleware
-	app.param('userId', users.userByID);
+  // Setting the github oauth routes
+  router.route('/auth/github').get(passport.authenticate('github'));
+  router.route('/auth/github/callback').get(users.oauthCallback('github'));
+
+  // Finish by binding the user middleware
+  router.param('userId', users.userByID);
 };
