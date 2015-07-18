c70d3e680b0e0231a5b5d781c6d65b91cb45e91e
skazska <skazska@gmail.com>
2015-07-18 10:30:48 +0500

    Record model tests

--- app/tests/record.server.model.test.js
+++ app/tests/record.server.model.test.js
@@ -5,13 +5,32 @@
  */
 var should = require('should'),
 	mongoose = require('mongoose'),
-	User = mongoose.model('User'),
-	Record = mongoose.model('Record');
+  async = require('async'),
+  errorHandler = require('../controllers/errors.server.controller'),
+  User = mongoose.model('User'),
+  Record = mongoose.model('Record');
+
 
 /**
  * Globals
  */
 var user, record;
+
+/**
+ * Record Schema
+ * This schema represents the record, record is the main data being of project
+ * It consists of:
+ * actualUntil - a date of expiration
+ * items - an Item reference list (Item is a classification atom,
+ * represented by an Item Schema) list should contain at least (one) Item
+ * values - list of subject data records relevant to the classification set
+ * -------------------
+ * created, user - technical fields
+ *
+ *
+ *
+ */
+
 
 /**
  * Unit tests
@@ -29,7 +48,6 @@ describe('Record Model Unit Tests:', function() {
 
 		user.save(function() { 
 			record = new Record({
-				name: 'Record Name',
 				user: user
 			});
 
@@ -38,22 +56,53 @@ describe('Record Model Unit Tests:', function() {
 	});
 
 	describe('Method Save', function() {
-		it('should be able to save without problems', function(done) {
-			return record.save(function(err) {
-				should.not.exist(err);
-				done();
-			});
-		});
-
-		it('should be able to show an error when try to save without name', function(done) { 
-			record.name = '';
-
-			return record.save(function(err) {
-				should.exist(err);
-				done();
-			});
-		});
+    it('should start with no records', function(done){
+      Record.find(function(err, itms) {
+        should.not.exist(err);
+        itms.should.have.length(0);
+        done();
+      });
+    });
+    it('should be able to show an error when try to save with no items or' +
+      ' values members', function(done) {
+      var records = [new Record({}), new Record({items:[]}), new Record({values:[]})];
+      async.each(
+        records,
+        function(rec){
+          rec.save(function(err) {
+            should.exist(err);
+            should(errorHandler.getErrorMessage(err)).match('Please fill');
+            should(errorHandler.getErrorMessage(err)).match(/items|values/);
+          });
+        },
+        function(err){
+          should.not.exist(err);
+          done();
+        }
+      );
+    });
 	});
+
+  it('should be able to show an error when try to save with no items or' +
+    ' values members', function(done) {
+    record.items = [];
+    record.values= [];
+    record.save(function(err) {
+      should.exist(err);
+      should(errorHandler.getErrorMessage(err)).match('Please assign items');
+      done();
+    });
+  });
+
+  it('should save with some items', function(done) {
+    record.items = ['Item1'];
+    record.values= [];
+    record.save(function(err) {
+      should.not.exist(err);
+      done();
+    });
+
+  });
 
 	afterEach(function(done) { 
 		Record.remove().exec();
