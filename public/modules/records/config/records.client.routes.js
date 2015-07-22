e8f619d9d9558f046e7a80f518b7df7e083b118f
skazska <skazska@gmail.com>
2015-07-22 17:40:08 +0500

    Work on #7, #9 work on templates, recordForm directive creation pending,  form markup moved from edit and create to  record.client.form.html

--- public/modules/records/config/records.client.routes.js
+++ public/modules/records/config/records.client.routes.js
@@ -8,32 +8,32 @@ angular.module('records').config(['$stateProvider',
 			.state('record', {
 				abstract: true,
 				url: '/records',
-				templateUrl: 'modules/records/views/option.client.view.html'
+				templateUrl: 'modules/records/views/record.client.view.html'
 			})
 			.state('record.list', {
 				url: '/list',
-				templateUrl: 'modules/records/views/list-option.client.view.html'
+				templateUrl: 'modules/records/views/list-record.client.view.html'
 			})
 			.state('record.create', {
 				url: '/create',
-				templateUrl: 'modules/records/views/create-option.client.view.html'
+				templateUrl: 'modules/records/views/edit-record.client.view.html'
 			})
 			.state('record.one', {
 				abstract: true,
 				url: '/:optionId',
-				templateUrl: 'modules/records/views/one-option.client.view.html'
+				templateUrl: 'modules/records/views/one-record.client.view.html'
 			})
 			.state('record.one.view', {
 				url: '/view',
-				templateUrl: 'modules/records/views/view-option.client.view.html'
+				templateUrl: 'modules/records/views/view-record.client.view.html'
 			})
 			.state('record.one.edit', {
 				url: '/edit',
-				templateUrl: 'modules/records/views/edit-option.client.view.html'
+				templateUrl: 'modules/records/views/edit-record.client.view.html'
 			})
 			.state('record.one.delete', {
 				url: '/delete',
-				templateUrl: 'modules/records/views/del-option.client.view.html'
+				templateUrl: 'modules/records/views/del-record.client.view.html'
 			})
 		;
 	}
